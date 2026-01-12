7dc528fa0050a8ed8732017f233e4bd300437d28
Erkan IŞIK <erkanisik@yahoo.com>
2026-01-12 23:42:02 +0300

    uyap version bump

--- akiskart/uyap/actions.py
+++ akiskart/uyap/actions.py
@@ -7,9 +7,10 @@
 from pisi.actionsapi import pisitools, shelltools, get
 
 WorkDir="."
+
 def setup():
-    shelltools.system("unzip uyap-pardus-5-4-14-amd6403-06-20251-41-pm.zip")
-    shelltools.system("ar xvf uyap_pardus_5_4_14_amd64/uyapeditor_5.4.14_amd64.deb")
+    shelltools.system("unzip uyap-pardus-5-4-16-amd6429-12-20252-41-pm.zip")
+    shelltools.system("ar xvf uyap_pardus_5_4_16_amd64/uyapeditor_5.4.16_amd64.deb")
     shelltools.system("tar xvf data.tar.xz")
 
 def install():
